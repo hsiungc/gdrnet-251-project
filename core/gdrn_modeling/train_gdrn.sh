27f002b906857047fd7840e64748c4ea7128acf9
Casey Hsiung <caseyhsiung@ATTMns4e6u.attlocal.net>
2023-03-02 00:47:54 -0800

    update python

--- core/gdrn_modeling/train_gdrn.sh
+++ core/gdrn_modeling/train_gdrn.sh
@@ -13,5 +13,5 @@ echo "use gpu ids: $CUDA_VISIBLE_DEVICES num gpus: $NGPU"
 NCCL_DEBUG=INFO
 OMP_NUM_THREADS=1
 MKL_NUM_THREADS=1
-CUDA_VISIBLE_DEVICES=$2 python core/gdrn_modeling/main_gdrn.py \
+CUDA_VISIBLE_DEVICES=$2 python3 core/gdrn_modeling/main_gdrn.py \
     --config-file $CFG --num-gpus $NGPU  ${@:3}
